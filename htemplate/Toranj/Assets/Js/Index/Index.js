3e8860c3527aac3be3d873b203d722957191e611
masoud-ahmadi <masoudahmadi441@gmail.com>
2026-08-01 19:07:27 +0330

    تغییرات ظاهری

--- htemplate/Toranj/Assets/Js/Index/Index.js
+++ htemplate/Toranj/Assets/Js/Index/Index.js
@@ -59,4 +59,90 @@ $(document).ready(function () {
     e.preventDefault();
     slidernew.slidePrev();
   });
+
+    const sliderdiscount = new Swiper('.discount_slider_product', {
+      lazy: {
+        loadPrevNext: true,
+        loadPrevNextAmount: 1,
+      },
+
+      freeMode: true,
+      loop: false,
+      rtl: true,
+      slidesPerView: 'auto',
+      spaceBetween: 15,
+
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      },
+
+      breakpoints: {
+        768: {
+          speed: 1000,
+        },
+      },
+    });
+
+    $('.btn-prev-discount').on('click', function (e) {
+      e.preventDefault();
+      sliderdiscount.slideNext();
+    });
+
+    $('.btn-next-discount').on('click', function (e) {
+      e.preventDefault();
+      sliderdiscount.slidePrev();
+    });
+
+     $(function () {
+       function startCounter($container) {
+         $container.find('.counter').each(function () {
+           let $this = $(this);
+
+           if ($this.hasClass('done')) return;
+
+           $this.addClass('done');
+
+           let target = parseInt($this.data('range'));
+           let $number = $this.find('.progress-number');
+
+           $({ Counter: 0 }).animate(
+             { Counter: target },
+             {
+               duration: 3000,
+               easing: 'swing',
+
+               step: function () {
+                 $number.text('+' + Math.ceil(this.Counter));
+               },
+
+               complete: function () {
+                 $number.text('+' + target);
+               },
+             }
+           );
+         });
+       }
+
+       const observer = new IntersectionObserver(
+         function (entries) {
+           entries.forEach(function (entry) {
+             if (entry.isIntersecting) {
+               startCounter($(entry.target));
+
+               // فقط همین سکشن از لیست observer حذف شود
+               observer.unobserve(entry.target);
+             }
+           });
+         },
+         {
+           threshold: 0.3,
+         }
+       );
+
+       $('.stats-counter').each(function () {
+         observer.observe(this);
+       });
+     });
 });
