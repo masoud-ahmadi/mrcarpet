484ed52cfd7d4b4790e99282a50b14534e49e584
masoud-ahmadi <masoudahmadi441@gmail.com>
2026-08-01 11:10:17 +0330

    ایجاد قسمت بهترین محصولات

--- htemplate/Toranj/Assets/Js/Index/Index.js
+++ htemplate/Toranj/Assets/Js/Index/Index.js
@@ -1,72 +1,62 @@
 $(document).ready(function () {
-  
-});
+  const swiper = new Swiper('.banner_slider', {
+    effect: 'cards',
+    grabCursor: true,
+    rtl: true,
+    speed: 1000,
+    autoHeight: true,
+    center: true,
+    cardsEffect: {
+      perSlideRotate: 2,
+      perSlideOffset: 5,
+      rotate: true,
+      slideShadows: false,
+    },
 
-const swiper = new Swiper('.banner_slider', {
-  effect: 'cards',
-  grabCursor: true,
-  rtl: true,
-  speed: 1000,
-  autoHeight: true,
-  center: true,
-  cardsEffect: {
-    perSlideRotate: 2,
-    perSlideOffset: 5,
-    rotate: true,
-    slideShadows: false,
-  },
-
-  lazy: {
-    loadPrevNext: true,
-    loadPrevNextAmount: 1,
-  },
-
-  autoplay: {
-    delay: 6000,
-    pauseOnMouseEnter: true,
-    disableOnInteraction: false,
-  },
-  
-
-});
-
-  const slidersidecontracting = new Swiper('.contracting_slider', {
-  
-    allowTouchMove: false,
-    
-    lazy: true,
     lazy: {
       loadPrevNext: true,
       loadPrevNextAmount: 1,
     },
-    slidesPerView: 'auto',
-    loop: true,
-    rtl: true,
-    spaceBetween: 10,
+
+    autoplay: {
+      delay: 6000,
+      pauseOnMouseEnter: true,
+      disableOnInteraction: false,
+    },
   });
 
-   const slidernew = new Swiper('.new_slider_product', {
-     lazy: {
-       loadPrevNext: true,
-       loadPrevNextAmount: 1,
-     },
+  const slidernew = new Swiper('.best_slider_product', {
+    lazy: {
+      loadPrevNext: true,
+      loadPrevNextAmount: 1,
+    },
 
-     freeMode: true,
-     loop: false,
-     rtl: true,
-     slidesPerView: 'auto',
-     spaceBetween: 20,
+    freeMode: true,
+    loop: false,
+    rtl: true,
+    slidesPerView: 'auto',
+    spaceBetween: 20,
 
-     autoplay: {
-       delay: 5000,
-       disableOnInteraction: false,
-       pauseOnMouseEnter: true,
-     },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
 
-     breakpoints: {
-       768: {
-         speed: 1000,
-       },
-     },
-   });
+    breakpoints: {
+      768: {
+        speed: 1000,
+      },
+    },
+  });
 
+  $('.btn-prev-best').on('click', function (e) {
+    e.preventDefault();
+    slidernew.slideNext();
+  });
+
+  $('.btn-next-best').on('click', function (e) {
+    e.preventDefault();
+    slidernew.slidePrev();
+  });
+});
